28f70aa31a7281a23fb75d11952accee4e529809
mick <mick@newmediafront.com>
2014-02-25 10:45:07 -0800

    added simple test data loader and members + tool categories data

---
+++ data/load.js
@@ -0,0 +1,64 @@
+var fs = require('fs')
+  , http = require('http')
+  
+var po = {
+    'host':'localhost', 
+    'port': '5984', 
+    'method': 'POST', 
+    'headers': {
+        'Content-Type': 'application/json'
+    } 
+}
+
+fs.readdir('.', function(err, files) {
+    if (err) {
+        throw err;
+    } else {
+        files.forEach(function(f) {
+            if (f.split(".")[1] == 'json') {
+                dropAndCreateDb(f.split(".")[0]);
+            }
+        });
+    }
+});
+
+var dropAndCreateDb = function(db)
+{
+    var req = http.request({'path':'/'+db, 'host':po.host, 'port': po.port, 'method': 'DELETE'}, function(res) {
+        res.on('data', function (chunk) {
+             console.log('Response: ' + chunk);
+             var req = http.request({'path':'/'+db, 'host':po.host, 'port': po.port, 'method': 'PUT'}, function(res) {
+                res.on('data', function (chunk) {
+                     console.log('Response: ' + chunk);
+                     loadData(db);
+                });
+             });
+             req.end();
+        });
+    });   
+    req.end();
+}
+
+var loadData = function(file) {
+    fs.readFile(file+'.json', 'utf8', function (err, data) {
+        if (err) {
+            throw err; 
+        } else {
+            data = JSON.parse(data);
+            for(var i=0;i<data.length; i++) {
+                po.path = '/'+file+'/'; 
+                var sd = JSON.stringify(data[i]);
+                po.headers["Content-Length"] =  Buffer.byteLength(sd)
+                var req = http.request(po, function(res) {
+                    res.setEncoding('utf8');
+                    res.on('data', function (chunk) {
+                        console.log('Response: ' + chunk);
+                    });
+                });
+                req.write(sd);
+                req.end();
+            };
+        }
+    });
+}
+
